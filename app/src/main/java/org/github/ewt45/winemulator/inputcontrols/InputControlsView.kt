38408c258e784a578ccb6f210e28a907b9ddc02c
afei <165532911+afeimod@users.noreply.github.com>
2026-05-08 19:35:18 +0800

    Add files via upload

--- app/src/main/java/org/github/ewt45/winemulator/inputcontrols/InputControlsView.kt
+++ app/src/main/java/org/github/ewt45/winemulator/inputcontrols/InputControlsView.kt
@@ -741,7 +741,8 @@ class InputControlsView(context: Context?) : View(context) {
                 val pointerButton = binding.getPointerButton()
                 if (isActionDown) {
                     if (pointerButton != null) {
-                        handler.onPointerButton(pointerButton.ordinal, true)
+                        // X11 按钮号从 1 开始，ordinal 是 0-based，所以要 +1
+                        handler.onPointerButton(pointerButton.ordinal + 1, true)
                     } else {
                         handler.onKeyEvent(binding.toEvdev(), true)
                     }
@@ -751,7 +752,8 @@ class InputControlsView(context: Context?) : View(context) {
                     }
                 } else {
                     if (pointerButton != null) {
-                        handler.onPointerButton(pointerButton.ordinal, false)
+                        // X11 按钮号从 1 开始，ordinal 是 0-based，所以要 +1
+                        handler.onPointerButton(pointerButton.ordinal + 1, false)
                     } else {
                         handler.onKeyEvent(binding.toEvdev(), false)
                     }
