d187274c06559fe2fcce0864e744cb7c1778ebf2
afei <165532911+afeimod@users.noreply.github.com>
2026-05-12 14:03:46 +0800

    Add files via upload

--- app/src/main/java/org/github/ewt45/winemulator/ui/ExpandableFloatingMenu.kt
+++ app/src/main/java/org/github/ewt45/winemulator/ui/ExpandableFloatingMenu.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.asImageBitmap
