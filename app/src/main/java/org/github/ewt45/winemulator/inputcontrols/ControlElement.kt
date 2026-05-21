2561c5595d2b64a6e9ee4fc9c869e62e19538367
afei <165532911+afeimod@users.noreply.github.com>
2026-05-21 16:03:01 +0800

    Add files via upload

--- app/src/main/java/org/github/ewt45/winemulator/inputcontrols/ControlElement.kt
+++ app/src/main/java/org/github/ewt45/winemulator/inputcontrols/ControlElement.kt
@@ -985,7 +985,7 @@ class ControlElement(
     }
 
     private fun getDarkColor(): Int {
-        val opacity = if (inputControlsView.isEditMode()) maxOf(0.15f, 1.0f) else 1.0f
+        val opacity = if (inputControlsView.editMode) maxOf(0.15f, 1.0f) else 1.0f
         return Color.argb((opacity * inputControlsView.overlayOpacity * 255).toInt(), 0, 0, 0)
     }
 
