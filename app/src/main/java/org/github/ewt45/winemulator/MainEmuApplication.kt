7fe578b8d10e9b6e1d6c38fdab77779446c22ede
afei <165532911+afeimod@users.noreply.github.com>
2026-05-31 17:55:53 +0800

    Add files via upload

--- app/src/main/java/org/github/ewt45/winemulator/MainEmuApplication.kt
+++ app/src/main/java/org/github/ewt45/winemulator/MainEmuApplication.kt
@@ -1,25 +1,31 @@
 package org.github.ewt45.winemulator
 
 import android.app.Application
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 
 
-class MainEmuApplication:Application() {
+class MainEmuApplication : Application() {
     companion object {
-        lateinit var  i:MainEmuApplication
+        lateinit var i: MainEmuApplication
+            private set
     }
+
     override fun onCreate() {
         super.onCreate()
-
         i = this
+        // 初始化 Consts
         Consts.init(this)
     }
 }
 
-private val MainEmuApplication.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
-/** 持久化数据。获取某个key的最新值可以通过Consts.Pref.xxx.get() */
-val dataStore = MainEmuApplication.i.dataStore
+// 使用顶层懒加载的 dataStore，基于 Application 实例
+// preferencesDataStore 是一个 Context 扩展属性
+private val Context._dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
+/**
+ * 获取 dataStore 实例，用于持久化数据存储
+ */
+val dataStore: DataStore<Preferences>
+    get() = MainEmuApplication.i._dataStore
