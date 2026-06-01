072ef7568bf2000476e7290d6e30773f1c6f6adc
afei <165532911+afeimod@users.noreply.github.com>
2026-06-01 13:24:31 +0800

    Add files via upload

---
+++ app/src/main/java/com/termux/x11/IILorieView.java
@@ -0,0 +1,9 @@
+package com.termux.x11;
+
+/**
+ * Local interface alias for ILorieView.
+ * This is used by the virtual controls system.
+ */
+public interface IILorieView extends ILorieView {
+    // Extends ILorieView for compatibility with the virtual controls system
+}
