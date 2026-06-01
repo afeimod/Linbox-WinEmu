a4060c2622537b32d6146255c358de91a8cdaa5a
afei <165532911+afeimod@users.noreply.github.com>
2026-06-01 18:07:32 +0800

    Add files via upload

--- app/src/main/java/com/termux/x11/controller/ControlsEditorActivity.java
+++ app/src/main/java/com/termux/x11/controller/ControlsEditorActivity.java
@@ -81,13 +81,20 @@ public class ControlsEditorActivity extends AppCompatActivity implements View.On
         inputControlsView.setOverlayOpacity(0.6f);
 
         profile = InputControlsManager.loadProfile(this, ControlsProfile.getProfileFile(this, getIntent().getIntExtra(EXTRA_PROFILE_ID, 0)));
-        
+
         if (profile == null) {
             AppUtils.showToast(this, R.string.no_profile_selected);
             finish();
             return;
         }
-        
+
+        // 确保 profile 名称不为 null
+        if (profile.getName() == null) {
+            AppUtils.showToast(this, R.string.no_profile_selected);
+            finish();
+            return;
+        }
+
         ((TextView) findViewById(R.id.TVProfileName)).setText(profile.getName());
         inputControlsView.setProfile(profile);
 
