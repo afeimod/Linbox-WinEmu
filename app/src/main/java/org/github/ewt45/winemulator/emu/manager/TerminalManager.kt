fb0305befc13ee951bae617dddd276554b6f1993
afei <165532911+afeimod@users.noreply.github.com>
2026-06-01 09:27:09 +0800

    Delete app/src/main/java/org/github/ewt45/winemulator directory

--- app/src/main/java/org/github/ewt45/winemulator/emu/manager/TerminalManager.kt
+++
@@ -1,4 +0,0 @@
-package org.github.ewt45.winemulator.emu.manager
-
-class TerminalManager {
-}
--- app/src/main/java/org/github/ewt45/winemulator/terminal/ViewClientImpl.kt
+++
@@ -1,9 +0,0 @@
-package org.github.ewt45.winemulator.terminal
-
-import org.github.ewt45.winemulator.MainEmuActivity
-
-class ViewClientImpl(
-    val activity: MainEmuActivity,
-    val sessionClient: SessionClientAImpl,
-): ViewClientBase() {
-}
